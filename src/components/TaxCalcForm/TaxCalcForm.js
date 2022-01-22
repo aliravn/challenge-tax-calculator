a7b8b8bfd460091c019f56497b1e042d37420434
aliravn <alira.vn@gmail.com>
2022-01-22 23:18:47 +0100

    add event handlers and tests for them

--- src/components/TaxCalcForm/TaxCalcForm.js
+++ src/components/TaxCalcForm/TaxCalcForm.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TaxCalcForm.css';
 
 const TaxCalcForm = () => {
+    const [income, setIncome] = useState('');
+    const [tax, setTax] = useState('Please type in you annual income');
+
+    const onIncomeValueChange = (event) => {
+            setIncome(event.target.value);
+    };
+
+    const calulateTax = () => {
+        // TODO: add correct calculation based on progressive taxation
+        // TODO: limit result to 2 digits after comma
+        const calcTax = income * 0.1;
+        setTax(calcTax);
+    }
     return (
         <section className='formContainer'>
             <label htmlFor="income" className='inputLabel'>Annual income:</label>
-            <input type="text" id="income" name="income" className='inputField'/>
+            <input 
+                type="text" 
+                id="income" 
+                name="income" 
+                value={income} 
+                onChange={onIncomeValueChange} 
+                className='inputField'
+            />
 
-            <button className='submitButton'>Calculate tax</button>
+            <button 
+                className='submitButton' 
+                onClick={calulateTax}
+            >
+               Calculate tax
+            </button>
+
+            <div className='result'>Tax to pay: {tax} EUR</div>
         </section>
     );
 }
--- src/components/TaxCalcForm/TaxCalcForm.test.js
+++ src/components/TaxCalcForm/TaxCalcForm.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import TaxCalcForm from './TaxCalcForm';
 
 describe('TaxCalcForm', () => {
@@ -11,5 +12,22 @@ describe('TaxCalcForm', () => {
 
         const submitButton = screen.getByText(/calculate tax/i)
         expect(submitButton).toBeInTheDocument();
-      });
+
+        const resultsArea = screen.getByText(/Tax to pay/i)
+        expect(resultsArea).toBeInTheDocument();
+    });
+
+
+    it('calculates the tax and shows the result', async () => {
+        render(<TaxCalcForm />);
+
+        const inputField = screen.getByLabelText(/annual income/i);
+        const submitButton = screen.getByText(/calculate tax/i);
+        const resultsArea = screen.getByText(/tax to pay/i);
+        userEvent.type(inputField, '100');
+        userEvent.click(submitButton);
+        expect(resultsArea).toHaveTextContent('10');
+
+        
+    });
 })
