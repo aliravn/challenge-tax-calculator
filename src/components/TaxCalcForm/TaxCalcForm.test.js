cfb8c4e7500faf767f265735725a81c40e74f536
aliravn <alira.vn@gmail.com>
2022-01-22 22:25:27 +0100

    extend test to check for submit button

--- src/components/TaxCalcForm/TaxCalcForm.test.js
+++ src/components/TaxCalcForm/TaxCalcForm.test.js
@@ -2,11 +2,12 @@ import { render, screen } from '@testing-library/react';
 import TaxCalcForm from './TaxCalcForm';
 
 describe('TaxCalcForm', () => {
-    it('renders TaxCalcForm', () => {
+    it('renders TaxCalcForm correctly', () => {
         render(<TaxCalcForm />);
 
         const inputField = screen.getByLabelText(/Annual income/i);
         expect(inputField).toBeInTheDocument();
+        expect(inputField).toHaveValue('');
 
         const submitButton = screen.getByText(/calculate tax/i)
         expect(submitButton).toBeInTheDocument();
