4ac39e1938ab51fc718246785647a5a4ca9fc8fa
aliravn <alira.vn@gmail.com>
2022-02-01 22:15:40 +0100

    add mock file for taxBracket

---
+++ src/components/TaxCalcForm/taxBracket.mock.js
@@ -0,0 +1,18 @@
+export const taxBracket = {
+    'low': {
+        'incomeCap': 10000,
+        'taxRate': 0.00,
+    },
+    'medium': {
+        'incomeCap': 30000,
+        'taxRate': 0.10,
+    },
+    'high': {
+        'incomeCap': 100000,
+        'taxRate': 0.25,
+    },
+    'open': {
+        'incomeCap': null,
+        'taxRate': 0.40,
+    },
+}
